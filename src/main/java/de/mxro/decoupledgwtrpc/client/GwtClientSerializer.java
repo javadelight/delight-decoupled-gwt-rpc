089a5e967cc75ec772070bea95bd1f6ddf9ea4ca
mxro <mxro@nowhere.com>
2014-01-20 17:48:44 +1300

    Working on new GWT RPC implementation Autocommit Mon Jan 20 17:48:44 NZDT 2014

--- src/main/java/de/mxro/decoupledgwtrpc/client/GwtClientSerializer.java
+++ src/main/java/de/mxro/decoupledgwtrpc/client/GwtClientSerializer.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.SerializationStreamFactory;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 
@@ -12,17 +11,23 @@ public class GwtClientSerializer {
 
 	private final Class<? extends RemoteService> baseService;
 	
-	public String serializeForServer(Serializable serializable)
-			throws SerializationException {
-		
+	public String serializeForServer(Serializable serializable) {
+		try {
 		SerializationStreamWriter writer = ((SerializationStreamFactory) GWT
 				.create(baseService)).createStreamWriter();
 		
 		writer.writeObject(serializable);
 		
 		return writer.toString();
+		} catch (Exception e ) {
+			throw new RuntimeException(e);
+		}
 	}
 
+	public String serializeForClient(Serializable serializable) {
+		
+	}
+	
 	public GwtClientSerializer(Class<? extends RemoteService> baseService) {
 		super();
 		this.baseService = baseService;
