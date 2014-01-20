1a7f24ce18696b9e1bc0012131a0a921fb055d92
mxro <mxro@nowhere.com>
2014-01-20 17:41:37 +1300

    a

---
+++ src/main/java/de/mxro/decoupledgwtrpc/server/GwtServerSerializer.java
@@ -0,0 +1,70 @@
+package de.mxro.decoupledgwtrpc.server;
+
+import com.google.gwt.user.client.rpc.impl.AbstractSerializationStream;
+import com.google.gwt.user.server.rpc.SerializationPolicy;
+import com.google.gwt.user.server.rpc.SerializationPolicyProvider;
+import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
+import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamWriter;
+
+public class GwtServerSerializer {
+
+	private final SerializationPolicy policy;
+	
+	/**
+	 * Needs message in format as described here
+	 * https://docs.google.com/document/d/1eG0YocsYYbNAtivkLtcaiEE5IOF5u4LUol8-LL0TIKU/edit
+	 * 
+	 * @param data
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T deserializeMessageFromClient(String data) {
+		try {
+
+			String content = data;
+
+			ServerSerializationStreamReader streamReader = new ServerSerializationStreamReader(
+					Thread.currentThread().getContextClassLoader(), new SerializationPolicyProvider() {
+						
+						@Override
+						public SerializationPolicy getSerializationPolicy(String moduleBaseURL,
+								String serializationPolicyStrongName) {
+							return policy;
+						}
+					});
+
+			streamReader.setFlags(AbstractSerializationStream.DEFAULT_FLAGS);
+			streamReader.prepareToRead(content);
+
+			return (T) streamReader.readObject();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String serializeForClient(Object obj) {
+		try {
+			
+			ServerSerializationStreamWriter stream = new ServerSerializationStreamWriter(
+					policy);
+			stream.setFlags(AbstractSerializationStream.DEFAULT_FLAGS);
+
+			stream.prepareToWrite();
+
+			stream.serializeValue(obj, Object.class);
+
+			return stream.toString();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public GwtServerSerializer(SerializationPolicy policy) {
+		super();
+		this.policy = policy;
+	}
+
+	
+	
+}
