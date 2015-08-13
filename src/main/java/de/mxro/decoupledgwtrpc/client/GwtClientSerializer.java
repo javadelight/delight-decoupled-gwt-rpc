6e288926eb038655bab24a4a86a93514fdbed0fb
mxro <mxro@nowhere.com>
2015-08-14 09:41:01 +1000

    Implementing Server-Side Procedures Autocommit Fri Aug 14 09:41:01 EST 2015

--- src/main/java/de/mxro/decoupledgwtrpc/client/GwtClientSerializer.java
+++ src/main/java/de/mxro/decoupledgwtrpc/client/GwtClientSerializer.java
@@ -1,15 +1,11 @@
 package de.mxro.decoupledgwtrpc.client;
 
-import delight.gwt.console.Console;
-
 import java.io.Serializable;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.SerializationException;
-import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamReader;
 import com.google.gwt.user.client.rpc.impl.Serializer;
 import com.seanchenxi.gwt.storage.client.serializer.ServerStorageRPCSerializerImpl;
-import com.seanchenxi.gwt.storage.client.serializer.StorageTypeSerializer;
 
 public class GwtClientSerializer {
 
@@ -23,7 +19,7 @@ public class GwtClientSerializer {
         }
     }
 
-    private final static Serializer TYPE_SERIALIZER = GWT.create(StorageTypeSerializer.class);
+    private final static Serializer TYPE_SERIALIZER = GWT.create(Serializer.class);
 
     /**
      * Does not accept string serialized for the server.
@@ -35,30 +31,9 @@ public class GwtClientSerializer {
         if (data == null) {
             throw new NullPointerException("String to be deserialized must not be null.");
         }
-        Console.log(data);
-        if (data.equals("")) {
-            throw new IllegalArgumentException("Data must not be empty string");
-        }
         try {
-            final String serializedString = data;
-            if (serializedString == null) {
-                return null;
-            } else if (String.class.equals(Serializable.class)) {
-                return serializedString;
-            }
-
-            final ClientSerializationStreamReader reader = new ClientSerializationStreamReader(TYPE_SERIALIZER);
-            Console.log("prepare to read " + data);
-            reader.prepareToRead(serializedString);
-            Console.log("reading it");
-            // Object obj =
-            // TYPE_SERIALIZER.get(Serializable.class).read(reader);
-            return reader.readObject();
-
-            // return this.clientSerializer.deserialize(Serializable.class,
-            // data);
+            return this.clientSerializer.deserialize(Serializable.class, data);
         } catch (final SerializationException e) {
-
             throw new RuntimeException(e);
         }
     }
