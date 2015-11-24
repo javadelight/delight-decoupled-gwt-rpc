01dce7435b92a1c3f22238f7f6ff38494b0b96bd
mxro <mxro@nowhere.com>
2015-11-24 15:11:47 +1100

    Implementing ObjectHub UI Autocommit Tue Nov 24 15:11:47 EST 2015

--- src/main/java/de/mxro/decoupledgwtrpc/client/GwtClientMessageSerializer.java
+++ src/main/java/de/mxro/decoupledgwtrpc/client/GwtClientMessageSerializer.java
@@ -1,5 +1,7 @@
 package de.mxro.decoupledgwtrpc.client;
 
+import delight.gwt.console.Console;
+
 import java.io.Serializable;
 
 import com.google.gwt.user.client.rpc.SerializationStreamFactory;
@@ -7,12 +9,14 @@ import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 
 public class GwtClientMessageSerializer {
 
-	Object serviceInstance;
-	
-	public String serializeForServer(Serializable serializable) {
+    Object serviceInstance;
+
+    public String serializeForServer(final Serializable serializable) {
 		try {
 			SerializationStreamWriter writer = ((SerializationStreamFactory) serviceInstance).createStreamWriter();
-
+			    
+			Console.log('serialize '+serializable.getClass());
+			
 			writer.writeObject(serializable);
 
 			return writer.toString();
@@ -21,15 +25,14 @@ public class GwtClientMessageSerializer {
 		}
 	}
 
-	/**
-	 * An instance of the RemoteService for which messages are to be generated.
-	 * @param serviceInstance
-	 */
-	public GwtClientMessageSerializer(Object serviceInstance) {
-		super();
-		this.serviceInstance = serviceInstance;
-	}
-	
-	
-	
+    /**
+     * An instance of the RemoteService for which messages are to be generated.
+     * 
+     * @param serviceInstance
+     */
+    public GwtClientMessageSerializer(final Object serviceInstance) {
+        super();
+        this.serviceInstance = serviceInstance;
+    }
+
 }
